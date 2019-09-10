a9e52df3d5c330eec08e11a21a2d2b8fe02efebf
yuanee <31670566+yuanee@users.noreply.github.com>
2019-09-10 14:41:15 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -28,38 +28,38 @@ if __name__ == "__main__":
 
 
 
-cst=str('Net')+str(args.N)+'L'+str(args.fold)+'.p'
-file=open(cst,'rb')
-Ct=pickle.load(file)
-Xarray=Ct['Xarray']
-RankPlack=Ct['Plack']
-RankMul=Ct['Mul']
-
-
-      
-Clist=[3**i for i in range(-20,10)]
-
-BetaMAPlist=[]
-BetaVIlist=[]
-Covlist=[]
-
-LowBoundList=[]
-for C_value in Clist:
-    ####the C_value is the prior distribution's 
-    length=np.shape(Xarray)[1]
-    Mn,Sn,lowbound=EMPlackett(Xarray,RankPlack,C_value,args.loopT)
-    # the function EMPlackett is the variational inference code to return mean covariance and lower bound
-    LowBoundList.append(lowbound)
-    Beta_MAP=MapEstimation(Xarray,RankMul,C_value)
-    # the MapEstimation can return the parameter estimation given by MAP method
-    BetaVIlist.append(Mn)
-    BetaMAPlist.append(Beta_MAP)
-    Covlist.append(Sn)
-
-Cp={}
-Cp['betamap']=BetaMAPlist
-Cp['betavi']=BetaVIlist  
-Cp['covvi']=Covlist
-Cp['lowbound']=LowBoundList
-csts='args.output'+str(args.N)+'Fold'+str(args.fold)+str('.p')
-pickle.dump(Cp,open(csts,"wb"))
+    cst=str('Net')+str(args.N)+'L'+str(args.fold)+'.p'
+    file=open(cst,'rb')
+    Ct=pickle.load(file)
+    Xarray=Ct['Xarray']
+    RankPlack=Ct['Plack']
+    RankMul=Ct['Mul']
+    
+    
+          
+    Clist=[3**i for i in range(-20,10)]
+    
+    BetaMAPlist=[]
+    BetaVIlist=[]
+    Covlist=[]
+    
+    LowBoundList=[]
+    for C_value in Clist:
+        ####the C_value is the prior distribution's 
+        length=np.shape(Xarray)[1]
+        Mn,Sn,lowbound=EMPlackett(Xarray,RankPlack,C_value,args.loopT)
+        # the function EMPlackett is the variational inference code to return mean covariance and lower bound
+        LowBoundList.append(lowbound)
+        Beta_MAP=MapEstimation(Xarray,RankMul,C_value)
+        # the MapEstimation can return the parameter estimation given by MAP method
+        BetaVIlist.append(Mn)
+        BetaMAPlist.append(Beta_MAP)
+        Covlist.append(Sn)
+    
+    Cp={}
+    Cp['betamap']=BetaMAPlist
+    Cp['betavi']=BetaVIlist  
+    Cp['covvi']=Covlist
+    Cp['lowbound']=LowBoundList
+    csts='args.output'+str(args.N)+'Fold'+str(args.fold)+str('.p')
+    pickle.dump(Cp,open(csts,"wb"))
